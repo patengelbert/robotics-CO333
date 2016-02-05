384468b16eb31d079cd664f68d8dd0414d762369
patengelbert <patengelbert@hotmail.co.uk>
2016-02-05 14:25:02 +0000

    Update newSquare.py
    Added clockwise/anticlockwise

--- BrickPi/newSquare.py
+++ BrickPi/newSquare.py
@@ -2,11 +2,15 @@ from robot import Robot
 
 repeats = int(input("Number of squares: "))
 size = float(input("Size of square (m): "))
+rot = bool(input("Anticlockwise?: ))
 with Robot() as robot:
 	for i in range(0, repeats):
 		for i in range(0, 4):
 			robot.move(size)
 			robot.wait()
-			robot.rotate(90)
+			if rot:
+				robot.rotate(90)
+			else:
+				robot.rotate(-90)
 			robot.wait()
 
