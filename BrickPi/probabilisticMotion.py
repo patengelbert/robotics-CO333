5ed7a7648e6e69a033f4eddd55a7858f06746cdd
Hamish Milne <hammil@theovermare.com>
2016-02-16 11:47:42 +0000

    Task 4 first part done

--- BrickPi/probabilisticMotion.py
+++ BrickPi/probabilisticMotion.py
@@ -1,5 +1,5 @@
 from robot import Robot
-from math import cos, sin
+from math import cos, sin, pi
 import random
 
 class Particle:
@@ -15,25 +15,35 @@ class ProbabilisticMotion:
 	step = 0.1
 	size = 0.4
 	numParticles = 100
+
+	scale = 400
+	offset = 100
 	
 	def __init__(self, robot):
 		self.robot = robot
 		self.particles = [Particle(0, 0, 0)]*self.numParticles
 
 	def noise(self):
-		return random.random()*0.05
+		return random.random()*0.01
 
 	def printParticles(self):
-		print("drawParticles:" + [(p.x, p.y, p.a) for p in self.particles])
+		scale = self.scale
+		offset = self.offset
+		print("drawParticles:" + str([(p.x*scale + offset, p.y*scale + offset, p.a) for p in self.particles]))
+
+	def printLine(self, x1, y1, x2, y2):
+		scale = self.scale
+		offset = self.offset
+		print("drawLine:" + str((int(x1*scale) + offset, int(y1*scale) + offset, int(x2*scale) + offset, int(y2*scale) + offset)))
 
 	def updateParticles(self, d):
-		particles = [Particle(\
+		self.particles = [Particle(\
 			p.x + (d + self.noise())*cos(p.a), \
 			p.y + (d + self.noise())*sin(p.a), \
 			p.a + self.noise()) for p in self.particles]
 
 	def updateAngle(self, a):
-		particles = [Particle(p.x, p.y, p.a + a + self.noise()) \
+		self.particles = [Particle(p.x, p.y, p.a + a + self.noise()) \
 			for p in self.particles]
 
 	def run(self):
@@ -47,16 +57,17 @@ class ProbabilisticMotion:
 				self.robot.wait()
 				self.updateParticles(self.step)
 				self.printParticles()
-				newX = posX + step*cos(angle)
-				newY = posY + step*sin(angle)
-				print("drawLine:" + str(posX, posY, newX, newY))
+				newX = posX + self.step*cos(angle)
+				newY = posY + self.step*sin(angle)
+				self.printLine(posX, posY, newX, newY)
 				posX = newX
 				posY = newY
-			self.robot.rotate(90)
+				total += self.step
+			self.robot.rotate(pi/2)
 			self.robot.wait()
-			self.updateAngle(90)
+			self.updateAngle(pi/2)
 			self.printParticles()
-			angle += 90
+			angle += pi/2
 
 if __name__ == '__main__':
 	robot = Robot()
