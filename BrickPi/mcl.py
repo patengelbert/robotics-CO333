f5c9ac1120efce4b4aff948cbe92e97b7eb1d9d4
patengelbert <patengelbert@hotmail.co.uk>
2016-02-23 11:40:07 +0000

    Added update to particles

--- BrickPi/mcl.py
+++ BrickPi/mcl.py
@@ -2,13 +2,15 @@ from robot import Robot
 from navigateToWaypoint import Navigate
 
 class MonteCarloWaypoint(Navigate):
+	
+	numParticles = 100
 
 	def __init__(self, robot):
-		self.robot = robot
+		super(Navigate).__init__(self, robot)
 		
 		#init for navigation
 		self.step = 20
-		self.particles = [Particle(0, 0, 0)]*self.numParticles
+		self.particles = [Particle(0, 0, 0, 1/sel.numParticles)]*self.numParticles
 	
 	def run(self):
 		running = true
@@ -22,11 +24,35 @@ class MonteCarloWaypoint(Navigate):
 				updatePoints()
 			
 
-	def updatePoints(self):
-		#self.particles = [Particle{\
-		#	p.x + (d + self.noise())*cos(p, a), \
-		#	p.y + (d + 
-		pass
+	def updatePoints(self, d, a):
+		# Redo weightings
+		self.particles = [Particle(\
+			p.x + (d + self.noise())*cos(p, a), \
+			p.y + (d + self.noise())*sin(p, a), \
+			p.a + self.noise() + a, \
+			p.p * calculate_likelihood(p.x, p.y, p.a, self.depth))\
+			for p in self.particles] 
+		# Resample high values and cull low values
+		self.particles = resample(self.particles)
+		# Normalise Weightings
+		self.particles = normalise(self.particles)
+		tX = 0, tY = 0, tA = 0
+		for p in self.particles:
+			tX += p.x
+			tY += p.y
+			tA += p.a
+		self.updatePosition(tA/self.numParticles, \
+			tX/self.numParticles, \
+			tY/self.numParticles)
+
+
+	def normalise(self, particles):
+		tWeight = 0
+		(tWeight += p.p) for p in particles
+		return [Particle(\
+			p.x, p.y, p.a,\
+			p.p / tWeight) \
+			for p in particles]
 		
 	def weightPoint(self, point):
 		pass # Weight points based on measured depth vs. mapped depth
