866748911c969f5de04a427907c23d68aa3c6c05
patengelbert <patengelbert@hotmail.co.uk>
2016-02-24 14:04:29 +0000

    Fixed minor bugs

--- BrickPi/mcl.py
+++ BrickPi/mcl.py
@@ -1,12 +1,13 @@
 from robot import Robot
 from navigateToWaypoint import Navigate
+from probabilisticMotion import Particle
 import random
 from math import exp, fabs, isinf
 from eventTypes import EventType
 
 
 class MonteCarloWaypoint(Navigate):
-	
+
 	numParticles = 100
 
 	def __init__(self, robot):
@@ -37,7 +38,6 @@ class MonteCarloWaypoint(Navigate):
 			
 			while self.x != pointX and self.y != pointY:
 				self.waypoint((pointX, pointY), self.step)
-				updatePoints()
 			
 
 	def updatePosition(self, d, a):
@@ -52,6 +52,7 @@ class MonteCarloWaypoint(Navigate):
 		self.particles = resample(self.particles)
 		# Normalise Weightings
 		self.particles = normalise(self.particles)
+		# Get mean of particles
 		tX = 0
 		tY = 0
 		tA = 0
@@ -64,11 +65,8 @@ class MonteCarloWaypoint(Navigate):
 		self.x = tX/self.numParticles
 		self.y = tY/self.numParticles
 
-
 	def normalise(self, particles):
-		tWeight = 0
-		for p in particles:
-			tWeight += p.p
+		tWeight = sum([p.p for p in particles])
 		return [Particle(\
 			p.x, p.y, p.a,\
 			p.p / tWeight) \
@@ -80,8 +78,7 @@ class MonteCarloWaypoint(Navigate):
 		if not isinf(measuredDepth):
 			exponent = fabs(estimatedDepth - measuredDepth)
 			return exp(-1 * exponent)
-		else:
-			return 1
+		return 1.0
 		
 	def resample(self):
 		# Normalise particle weightings
@@ -128,4 +125,4 @@ class MonteCarloWaypoint(Navigate):
 if __name__ == '__main__':
 	robot = Robot()
 	robot.events.add(EventType.SENSOR_ULTRASOUND, onUltrasound)
-	# TODO see if we ca change robot to use a behaviour rather than a behaviour use a robot
+	# TODO see if we can change robot to use a behaviour rather than a behaviour use a robot
